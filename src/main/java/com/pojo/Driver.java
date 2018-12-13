8a5d1df0fd7d837edb44f4f0aa1aabd015a82632
knowledges <qbl378739311@163.com>
2018-12-13 17:33:11 +0800

    实体类
    创建司机实体类

---
+++ src/main/java/com/pojo/Driver.java
@@ -0,0 +1,58 @@
+package com.pojo;
+
+public class Driver {
+    private String id;
+    private String userName;
+    private String password;
+    private String phone;
+    private Boolean isAuthentication;
+    private DateTime createTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Boolean getAuthentication() {
+        return isAuthentication;
+    }
+
+    public void setAuthentication(Boolean authentication) {
+        isAuthentication = authentication;
+    }
+
+    public DateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(DateTime createTime) {
+        this.createTime = createTime;
+    }
+}
